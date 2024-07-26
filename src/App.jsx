92cf13e67da8f2d0d1fcce25b4322c6298219e42
ValeriCodeU <valeri.yanev@icloud.com>
2024-07-27 02:29:32 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import UserList from './components/UserList.jsx'
+
+function App() {
+
+    return (
+
+        <div>
+            <Header />
+
+            <main className="main">
+                <UserList />
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+    return (
+        <footer className="footer">
+            <p>This site is designed to be used for training purposes at SoftUni.</p>
+        </footer >
+    );
+};
+
+export default Footer;
+
+// export default function Footer() {
+//     return (
+//         <footer className="footer">
+//         <p>This site is designed to be used for training purposes at SoftUni.</p>
+//     </footer >
+//     );
+// }
